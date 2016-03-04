6e090b51120695fb61eba6c3ab127b725458655f
WangPan <wangpantech@outlook.com>
2016-03-04 17:51:17 +0800

    GCD test

--- WhoIsTheRichest/ViewController.m
+++ WhoIsTheRichest/ViewController.m
@@ -32,15 +32,7 @@
  
     NSLog(@"%p , %p,  %p , %p , %@",m1,m2,m3,m4,xtr);
     
-    NSString * url = @"http://op.juhe.cn/onebox/weather/query";
-    NSMutableDictionary * dict = [NSMutableDictionary dictionaryWithObject:@"北京" forKey:@"cityname"];
-    [dict setObject:@"6925c4934f3624cc823f5addaee0a091" forKey:@"key"];
-    [[NetManager shareManager] POST:url parameters:dict success:^(id responsed) {
-        NSDictionary * dict = responsed;
-        DELog(@"%@",dict);
-    } failure:^(NSError *error) {
-        
-    }];
+
     
     BOOL res1 = [(id)[NSObject class] isKindOfClass:[NSObject class]];
     
@@ -67,27 +59,134 @@
     NSString * getStr =   object_getIvar(manager, name_ivar);
     NSLog(@"%@",getStr);
     // Do any additional setup after loading the view, typically from a nib.
-    
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        
-        
-        dispatch_async(dispatch_get_main_queue(), ^{
-            
-            
-        });
-        
+    dispatch_queue_t aqueue  = dispatch_get_main_queue();
+
+    dispatch_async(aqueue, ^{
+        [self printHD];
+        DELog(@"end sy");
+    });
+    dispatch_async(aqueue, ^{
+        [self printSD];
+        DELog(@"end--asy");
     });
     
-    NSString * string1 =                                                                            @"218140758923475009028430679850461237";
-    NSString * string2 =  @"52431537641987205601562093650123700561270354718932678541907238647813252547";
-
-    CTest * ct = [[CTest alloc] init];
-    NSString * plus = [ct plusBy:string1 and:string2];
-    DELog(@"%@",plus);
-    NSString * multi = [ct multiByString:string1 and:string2];
-    DELog(@"%@",multi);
+    
+//    NSString * string1 =                                                                            @"218140758923475009028430679850461237";
+//    NSString * string2 =  @"52431537641987205601562093650123700561270354718932678541907238647813252547";
+//
+//    CTest * ct = [[CTest alloc] init];
+//    NSString * plus = [ct plusBy:string1 and:string2];
+//    DELog(@"%@",plus);
+//    NSString * multi = [ct multiByString:string1 and:string2];
+//    DELog(@"%@",multi);
+   
+//    NSBlockOperation * bop = [NSBlockOperation blockOperationWithBlock:^{
+//        DELog(@"bop create");
+//    }];
+//    [bop addExecutionBlock:^{
+//        for (int i  = 0; i < 100; i ++) {
+//            DELog(@"IS - %d---- bop",i);
+//        }
+//    }];
+//    
+//    [bop addExecutionBlock:^{
+//        [self request];
+//    }];
+//    
+//    NSBlockOperation * cbop = [NSBlockOperation blockOperationWithBlock:^{
+//        DELog(@"ccccbop create");
+//    }];
+//    [cbop addDependency:bop];
+//    NSOperationQueue * queue = [[NSOperationQueue alloc] init];
+//    queue.maxConcurrentOperationCount = 1;
+//    [queue addOperations:@[bop ,cbop] waitUntilFinished:YES];
+//    
+//    
+    
+//    dispatch_group_t  group = dispatch_group_create();
+//    dispatch_queue_t aqueue  = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+//    
+//    
+//    dispatch_group_async(group, aqueue, ^{
+//        [self printHD];
+//    });
+//    
+//    dispatch_group_async(group, aqueue, ^{
+//        [self request];
+//
+//    });
+//    
+//    dispatch_group_notify(group, aqueue, ^{
+//        DELog(@"end")
+//    });
+//    
+    
+//    NSInvocationOperation * rqop = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(request) object:nil];
+//    NSInvocationOperation *hdop = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(printHD) object:nil];
+//    NSInvocationOperation * sdop = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(printSD) object:nil];
+//    
+//    [hdop addDependency:rqop];
+//    [sdop addDependency:hdop];
+//    
+//    NSOperationQueue * queue = [[NSOperationQueue alloc] init];
+//    [queue addOperations:@[rqop,sdop,hdop] waitUntilFinished:YES];
+//    dispatch_queue_t  myQueue = dispatch_queue_create("myQueue.someClass.myApp", DISPATCH_QUEUE_SERIAL);
+//    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+//        dispatch_async(myQueue, ^{
+//            [self request];
+//            DELog(@"hihihi");
+//        });
+//        dispatch_async(myQueue, ^{
+//            for (int i = 0; i < 100; i++) {
+//                DELog(@"---%d",i );
+//            }
+//            DELog(@"hahahahhaha");
+//        });
+//        
+//        dispatch_async(myQueue, ^{
+//            for (int i = 0; i < 1011; i ++) {
+//                DELog(@"%d",i );
+//            }
+//            DELog(@"hehehehe");
+//        });
+//    });
+//
+    DELog(@"endtototo");
+//    
+//    DELog(@"sync - %@",[NSThread currentThread]);
+//    dispatch_sync(dispatch_get_main_queue(), ^{
+//        DELog(@"sync - %@",[NSThread currentThread]);
+//    });
+//    DELog(@"sync - %@",[NSThread currentThread]);
 }
 
+-(void)printHD{
+    for (int i = 0; i < 10000; i ++) {
+        DELog(@"100-----%d",i);
+    }
+    DELog(@"end ----------------------------------------------------------------100");
+}
+-(void)printSD{
+    for (int i = 0; i < 10000; i ++) {
+        DELog(@"1000-----%d",i);
+    }
+    DELog(@"end ----------------------------------------------------------------1000");
+
+}
+-(void)request{
+    
+    DELog(@"req ----------------------------------------------------------------start");
+
+    NSString * url = @"http://op.juhe.cn/onebox/weather/query";
+    NSMutableDictionary * dict = [NSMutableDictionary dictionaryWithObject:@"北京" forKey:@"cityname"];
+    [dict setObject:@"6925c4934f3624cc823f5addaee0a091" forKey:@"key"];
+    [[NetManager shareManager] POST:url parameters:dict success:^(id responsed) {
+        NSDictionary * dict = responsed;
+        DELog(@"%@",dict);
+    } failure:^(NSError *error) {
+        
+    }];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
